b2ef58b29fc0974f80a81ca9df25d1038bbb0bda
scottf007 <scott@fletchcorp.com>
2017-02-23 23:32:56 +1100

    Update validate-rsync.sh

--- server/validate-rsync.sh
+++ server/validate-rsync.sh
@@ -1,18 +1,34 @@
-﻿#!/bin/sh
+#!/bin/sh 
 
-ROOTDIR=$1
-
-case "$SSH_ORIGINAL_COMMAND" in
-    *\&*)
-        echo "Rejected"
-        ;;
-    *\;*)
-        echo "Rejected"
-        ;;
-    rsync\ --server*$ROOTDIR*)
-        $SSH_ORIGINAL_COMMAND
-        ;;
-    *)
-        echo "Rejected"
-        ;;
-esac
+case "$SSH_ORIGINAL_COMMAND" in 
+*\&*) 
+echo "Rejected" 
+;; 
+*\(*) 
+echo "Rejected" 
+;; 
+*\{*) 
+echo "Rejected" 
+;; 
+*\;*) 
+echo "Rejected" 
+;; 
+*\<*) 
+echo "Rejected" 
+;; 
+*\>*) 
+echo "Rejected" 
+;; 
+*\`*) 
+echo "Rejected" 
+;; 
+*\|*) 
+echo "Rejected" 
+;; 
+rsync\ --server*) 
+$SSH_ORIGINAL_COMMAND 
+;; 
+*) 
+echo "Rejected" 
+;; 
+esac 
